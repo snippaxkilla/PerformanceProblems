0c22180f06a92fc790f5d77edadcd693b25e321e
Jack Lin <43402335+snippaxkilla@users.noreply.github.com>
2024-02-25 22:06:17 +0100

    don't call getcomponent repeaditly

--- Assets/Scripts/BeeAnimScript.cs
+++ Assets/Scripts/BeeAnimScript.cs
@@ -1,34 +1,42 @@
 ﻿using UnityEngine;
 
-public class BeeAnimScript : MonoBehaviour {
+public class BeeAnimScript : MonoBehaviour
+{
 
-    GameObject[] bee;
-    float dist = 0;
-    Vector3 middle = new Vector3(5f, 2f, 5f);
+    private Rigidbody rb;
+    private GameObject[] bees;
+    private float dist = 0;
+    private Vector3 middle = new Vector3(5f, 2f, 5f);
 
-    void FixedUpdate() {
-        //grab array
-        bee = BB.bees.bee;
-        // 1st, go to ONE point
-        Vector3 oink = Vector3.ClampMagnitude(middle - transform.position, 1);
-        GetComponent<Rigidbody>().AddForce(oink * 2f, ForceMode.Acceleration);
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
-        for (int i = 0; i < BB.bees.nBees; i++) {
-            //do not check self!
-            if (bee[i] != this.gameObject) {
-                //distance check:
-                dist = Vector3.Distance(transform.position, bee[i].transform.position);
-                if (dist < BB.bees.spacing) {
-                    Vector3 targetDirection = Vector3.ClampMagnitude(transform.position - bee[i].transform.position, 1);
-                    GetComponent<Rigidbody>().AddForce(targetDirection * 3f, ForceMode.Acceleration);
+    private void FixedUpdate()
+    {
+        bees = BB.bees.bee;
+        Vector3 directionToMiddle = Vector3.ClampMagnitude(middle - transform.position, 1);
+        rb.AddForce(directionToMiddle * 2f, ForceMode.Acceleration);
+
+        for (int i = 0; i < BB.bees.nBees; i++)
+        {
+            if (bees[i] != gameObject)
+            {
+                dist = Vector3.Distance(transform.position, bees[i].transform.position);
+                if (dist < BB.bees.spacing)
+                {
+                    Vector3 separationForce = Vector3.ClampMagnitude(transform.position - bees[i].transform.position, 1);
+                    rb.AddForce(separationForce * 3f, ForceMode.Acceleration);
                 }
             }
         }
-        //finally, avoid Queen Bee
+
         dist = Vector3.Distance(transform.position, BB.bees.queen.transform.position);
-        if (dist < BB.bees.spacing*2.5f) {
-            Vector3 targetDirection = Vector3.ClampMagnitude(transform.position - BB.bees.queen.transform.position, 1);
-            GetComponent<Rigidbody>().AddForce(targetDirection * 3f, ForceMode.Acceleration);
+        if (dist < BB.bees.spacing * 2.5f)
+        {
+            Vector3 avoidQueenForce = Vector3.ClampMagnitude(transform.position - BB.bees.queen.transform.position, 1);
+            rb.AddForce(avoidQueenForce * 3f, ForceMode.Acceleration);
         }
-    }      
+    }
 }
